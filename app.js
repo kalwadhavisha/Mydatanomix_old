7e8f62d1ff3ba6e7b6169d2955a269d8576d6da1
Havisha Kalwad <40450760+kalwadhavisha@users.noreply.github.com>
2020-10-25 13:49:22 -0700

    FInal Commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const CompanyOverview = require("./models/setting-schema");
+var numeral = require("numeral");
+var moment = require("moment");
+const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
@@ -12,6 +15,7 @@ app.use(morgan("dev"));
 const apiKey = "TBFKVIJZT7IK5VTB";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const dbURL =
@@ -83,79 +87,33 @@ app.get("/", function (req, res) {
 	console.log("In get");
 });
 
-app.get("/company-search", function (req, res) {
-	/*
-	const companyOverview = new CompanyOverview({
-		Symbol: "PYPL",
-		AssetType: "Common Stock",
-		Name: "PayPal Holdings, Inc",
-		Description:
-			"PayPal Holdings, Inc. operates as a technology platform and digital payments company that enables digital and mobile payments on behalf of consumers and merchants worldwide. Its payment solutions include PayPal, PayPal Credit, Braintree, Venmo, Xoom, and iZettle products. The company's payments platform allows consumers to send and receive payments, withdraw funds to their bank accounts, and hold balances in their PayPal accounts in various currencies. It also offers gateway services that enable merchants to accept payments online with credit or debit cards, as well as digital wallets. PayPal Holdings, Inc. was founded in 1998 and is headquartered in San Jose, California.",
-		Exchange: "NASDAQ",
-		Currency: "USD",
-		Country: "USA",
-		Sector: "Financial Services",
-		Industry: "Credit Services",
-		Address: "2211 North First Street, San Jose, CA, United States, 95131",
-		FullTimeEmployees: "23200",
-		FiscalYearEnd: "December",
-		LatestQuarter: "2020-06-30",
-		MarketCapitalization: "238883880960",
-		EBITDA: "3660000000",
-		PERatio: "90.4908",
-		PEGRatio: "2.0802",
-		BookValue: "15.038",
-		DividendPerShare: "None",
-		DividendYield: "0",
-		EPS: "2.179",
-		RevenuePerShareTTM: "16.37",
-		ProfitMargin: "0.1344",
-		OperatingMarginTTM: "0.1516",
-		ReturnOnAssetsTTM: "0.0327",
-		ReturnOnEquityTTM: "0.1527",
-		RevenueTTM: "19217999872",
-		GrossProfitTTM: "7987000000",
-		DilutedEPSTTM: "2.179",
-		QuarterlyEarningsGrowthYOY: "0.87",
-		QuarterlyRevenueGrowthYOY: "0.222",
-		AnalystTargetPrice: "220.2",
-		TrailingPE: "90.4908",
-		ForwardPE: "44.0529",
-		PriceToSalesRatioTTM: "12.1793",
-		PriceToBookRatio: "13.1211",
-		EVToRevenue: "11.8297",
-		EVToEBITDA: "49.4009",
-		Beta: "1.1131",
-		"52WeekHigh": "212.45",
-		"52WeekLow": "82.07",
-		"50DayMovingAverage": "193.492",
-		"200DayMovingAverage": "164.6696",
-		SharesOutstanding: "1173299968",
-		SharesFloat: "1170894503",
-		SharesShort: "17283459",
-		SharesShortPriorMonth: "14160002",
-		ShortRatio: "1.9",
-		ShortPercentOutstanding: "0.01",
-		ShortPercentFloat: "0.0147",
-		PercentInsiders: "0.127",
-		PercentInstitutions: "86.617",
-		ForwardAnnualDividendRate: "0",
-		ForwardAnnualDividendYield: "0",
-		PayoutRatio: "0",
-		DividendDate: "None",
-		ExDividendDate: "None",
-		LastSplitFactor: "None",
-		LastSplitDate: "None",
-	});
-	companyOverview
-		.save()
-		.then((result) => {
-			res.send(result);
+app.get("/viz-page", function (req, res) {
+	console.log("Viz page requested");
+	console.log(req.body.name);
+	var names;
+	var symbols;
+	var marketCap;
+	CompanyOverview.find({}, { Symbol: 1, Name: 1, MarketCapitalization: 1 })
+		.sort({
+			MarketCapitalization: -1,
 		})
-		.catch((err) => {
-			console.log(err);
+		.limit(10)
+		.then((result) => {
+			names = result.map((i) => i.Name);
+			symbols = result.map((i) => i.Symbol);
+			marketCap = result.map((i) => parseInt(i.MarketCapitalization));
+			//console.log(names);
+			//console.log(symbols);
+			//console.log(marketCap);
+			res.render("visualization", {
+				compSymbols: symbols,
+				compLongNames: names,
+				compDataVal: marketCap,
+			});
 		});
-	*/
+});
+
+app.get("/company-search", function (req, res) {
 	res.render("index", {
 		companyName: null,
 		companyTicker: null,
@@ -226,32 +184,34 @@ app.post("/company-search", function (req, res) {
 				companyForwardPE: result[0].ForwardPE,
 				companyAssetType: result[0].AssetType,
 				companyExchange: result[0].Exchange,
-				companyLatestQuarter: result[0].LatestQuarter,
+				companyLatestQuarter: moment(result[0].LatestQuarter).format("LL"),
 				companyPERatio: result[0].PERatio,
 				companyPEGRatio: result[0].PEGRatio,
 				companyPriceToSales: result[0].PriceToSalesRatioTTM,
 				companyPriceToBook: result[0].PriceToBookRatio,
 				companyEVToRevenue: result[0].EVToRevenue,
 				companyEVTOEBITDA: result[0].EVToEBITDA,
-				companyMarketCap: result[0].MarketCapitalization,
-				companyEBITDA: result[0].EBITDA,
+				companyMarketCap: numeral(result[0].MarketCapitalization).format("(0.000 a)"),
+				companyEBITDA: numeral(result[0].EBITDA).format("(0.000 a)"),
 				companyBookValue: result[0].BookValue,
 				companyEPS: result[0].EPS,
-				companyRevenuePerShare: result[0].RevenuePerShareTTM,
+				companyRevenuePerShare: parseFloat(result[0].RevenuePerShareTTM),
 				companyProfitMargin: result[0].ProfitMargin,
 				companyOperatingMargin: result[0].OperatingMarginTTM,
 				companyReturnOnAssets: result[0].ReturnOnAssetsTTM,
 				companyReturnOnEquity: result[0].ReturnOnEquityTTM,
-				companyRevenue: result[0].RevenueTTM,
-				companyGrossProfit: result[0].GrossProfitTTM,
+				companyRevenue: numeral(result[0].RevenueTTM).format("(0.000 a)"),
+				companyGrossProfit: numeral(result[0].GrossProfitTTM).format("(0.000 a)"),
 				companyDilutedEPS: result[0].DilutedEPSTTM,
 				companyQuarterlyEarningsYOY: result[0].QuarterlyEarningsGrowthYOY,
 				companyQuarterlyRevenueYOY: result[0].QuarterlyRevenueGrowthYOY,
 				companyBeta: result[0].Beta,
-				companySharesOutstanding: result[0].SharesOutstanding,
-				companySharesFloat: result[0].SharesFloat,
-				companySharesShort: result[0].SharesShort,
-				companySharesShortPriorMonth: result[0].SharesShortPriorMonth,
+				companySharesOutstanding: numeral(result[0].SharesOutstanding).format("(0.000 a)"),
+				companySharesFloat: numeral(result[0].SharesFloat).format("(0.000 a)"),
+				companySharesShort: numeral(result[0].SharesShort).format("(0.000 a)"),
+				companySharesShortPriorMonth: numeral(result[0].SharesShortPriorMonth).format(
+					"(0.000 a)"
+				),
 				companyShortRatio: result[0].ShortRatio,
 				companyShortPercentOutstanding: result[0].ShortPercentOutstanding,
 				companyShortPercentFloat: result[0].ShortPercentFloat,
@@ -260,24 +220,13 @@ app.post("/company-search", function (req, res) {
 				companyForwardAnnualDividendRate: result[0].ForwardAnnualDividendRate,
 				companyForwardAnnualDividendYield: result[0].ForwardAnnualDividendYield,
 				companyPayoutRatio: result[0].PayoutRatio,
-				companyDividendDate: result[0].DividendDate,
-				companyExDividendDate: result[0].ExDividendDate,
+				companyDividendDate: moment(result[0].DividendDate).format("LL"),
+				companyExDividendDate: moment(result[0].ExDividendDate).format("LL"),
 				companyLastSplitFactor: result[0].LastSplitFactor,
-				companyLastSplitDate: result[0].LastSplitDate,
+				companyLastSplitDate: moment(result[0].LastSplitDate).format("LL"),
 				companyDividendPerShare: result[0].DividendPerShare,
 				companyDividendYield: result[0].DividendYield,
 			});
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-
-	CompanyOverview.find({}, { Symbol: 1, PEGRatio: 1 })
-		.sort({ PEGRatio: -1 })
-		.collation({ locale: "en_US", numericOrdering: true })
-		.then((val) => {
-			console.log("**************************************");
-			console.log(val);
 		})
 		.catch((err) => {
 			console.log(err);
